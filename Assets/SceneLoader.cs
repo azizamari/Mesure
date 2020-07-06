0d824532d3f8750d209cb7d90ec02a644765600e
azizamari <azizamari2003@gmail.com>
2020-07-06 16:31:13 +0100

    mesure

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMesureScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
